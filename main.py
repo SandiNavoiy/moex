1260dd8c9e841110aac24602ecf376826a26a7c1
Sandi_Navoiy <santes.au@gmail.com>
2023-08-27 15:03:37 +0300

    1

--- main.py
+++ main.py
@@ -10,3 +10,18 @@ with requests.Session() as session:
     print(df.head(), '\n')
     print(df.tail(), '\n')
     df.info()
+
+request_url = ('https://iss.moex.com/iss/engines/stock/'
+               'markets/shares/boards/TQBR/securities.json')
+arguments = {'securities.columns': ('SECID,'
+                                    'REGNUMBER,'
+                                    'LOTSIZE,'
+                                    'SHORTNAME')}
+with requests.Session() as session:
+    iss = apimoex.ISSClient(session, request_url, arguments)
+    data = iss.get()
+    df = pd.DataFrame(data['securities'])
+    df.set_index('SECID', inplace=True)
+    print(df.head(), '\n')
+    print(df.tail(), '\n')
+    df.info()
